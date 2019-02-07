7859aed912bbb0393b1b0c5f7b70642a8d6b63ba
Tom Hicks <headhunter3@gmail.com>
2019-02-07 05:52:38 +0000

    Adds cors middleware to express app. Adds basic crud form to angular client.

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+class User {
+  public name: string;
+  public age: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-client';
+export class AppComponent implements OnInit {
+  title = 'app works!';
+
+  // Link to our api, pointing to localhost
+  API = 'http://localhost:3000';
+
+  // Declare empty list of people
+  people: any[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.getAllPeople();
+  }
+
+  // Add one person to the API
+  addPerson(name, age) {
+    this.httpClient.post(`${this.API}/users`, {name, age})
+      .subscribe(() => {
+        this.getAllPeople();
+      });
+  }
+
+  // Get all users from the API
+  getAllPeople() {
+    this.httpClient.get<User[]>(`${this.API}/users`)
+    .subscribe((people) => {
+      console.dir(people);
+      this.people = people;
+    });
+  }
 }
